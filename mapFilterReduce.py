fe0c4cc732678ac894da1cadfe397c36914267d7
emeceerre <emeceerre@gmail.com>
2019-04-23 20:39:21 +0200

    Incluyo ejemplo con reduce

--- mapFilterReduce.py
+++ mapFilterReduce.py
@@ -1,3 +1,5 @@
+import functools
+
 lista = [1, 3,-1, 15, 9]
 lista_dobles = map(lambda x: x*2, lista)
 print('Lista de dobles: ', list(lista_dobles))
@@ -5,5 +7,29 @@ print('Lista de dobles: ', list(lista_dobles))
 lista_pares = filter(lambda x: x%2==0, lista)
 print('Lista de pares: ', list(lista_pares))
 
-for va in lista_dobles:
-	print('holi', va)
+def SumatorioDoble(l):
+	resultado = 0
+	for valor in l:
+		resultado += valor*2
+	return resultado
+
+def ProductoTotal(l):
+	resultado = 1
+	for valor in l:
+		resultado *= valor
+	return resultado
+
+lista = [1, 3, -1, 15, 9]
+
+sumatorio = functools.reduce(lambda x, y: x+y, lista)
+
+# creo una copia de la lista
+l = lista[:]
+# añado el neutro de la suma en la posición cero
+l.insert(0,0)
+sumatorioDobles = functools.reduce(lambda x, y: x + y*2, l)
+sumatorioDoblesClasico = SumatorioDoble(lista)
+
+print(sumatorio)
+print(sumatorioDobles)
+print(sumatorioDoblesClasico)
