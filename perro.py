6500ae22bfa65dea66bb535fa8fed76b91e4924b
emeceerre <emeceerre@gmail.com>
2019-04-24 21:17:03 +0200

    Incluyo los metodos ladrar y __str__ en la clase Perro

--- perro.py
+++ perro.py
@@ -4,5 +4,20 @@ class Perro():
 		self.edad = e
 		self.peso = p
 
-	
+	def ladrar(self):
+		if self.peso >= 8:
+			print('GUAU, GUAU!')
+		else:
+			print('guau, guau!')
 
+	def __str__(self):
+		return ("Soy el perro {}, con {} años y peso {} kilos.".format(
+			self.nombre, self.edad, self.peso))
+
+salchicho = Perro('Salchicho',3,12)
+print(salchicho.nombre)
+salchicho.ladrar()
+lola = Perro('Lola', 8, 1.5)
+lola.ladrar()
+miko = Perro('Miko', 8, 3)
+print(salchicho)
